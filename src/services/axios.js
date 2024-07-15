bc9457aea45ae809e2a92c645560319836940650
nurcan <nurcancurebal@hotmail.com>
2024-07-15 22:17:52 +0300

    axios

---
+++ src/services/axios.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import router from "../router";
+
+const baseURL = process.env.VUE_APP_BASE_URL;
+
+const instance = axios.create({
+  baseURL: baseURL,
+});
+
+instance.interceptors.request.use((config) => {
+  const authToken = localStorage.getItem("token");
+
+  if (authToken) {
+    config.headers.Authorization = `Bearer ${authToken}`;
+  }
+  return config;
+});
+
+instance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error?.response?.status == 401) {
+      localStorage.removeItem("token");
+      router.push("SignIn");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,7 @@
 // ? Node modules.
-import Vue from 'vue';
-import Vuex from 'vuex';
-import axios from "axios";
-
-axios.interceptors.request.use(config => {
-
-  const authToken = localStorage.getItem('token');
-
-  if (authToken) {
-
-    config.headers.Authorization = `Bearer ${authToken}`;
-  };
-  return config;
-});
-
-axios.interceptors.response.use(function (response) {
-
-  return response;
-
-}, function (error) {
-
-  if (error?.response?.status == 401) {
-
-    localStorage.removeItem("token");
-    this.$router.push("SignIn");
-  };
-  return Promise.reject(error);
-});
+import Vue from "vue";
+import Vuex from "vuex";
+import instance from "@/services/axios";
 
 // ? Inits.
 Vue.use(Vuex);
@@ -38,229 +13,179 @@ const state = {
 };
 
 const getters = {
-
   todo(state) {
-
-    return state.todo
+    return state.todo;
   },
 
   user(state) {
-    return state.user
-  }
+    return state.user;
+  },
 };
 
 const mutations = {
-
   TODO(state, context) {
-
     state.todo = context;
   },
 
   USER(state, context) {
-    state.user = context
-  }
+    state.user = context;
+  },
 };
 
 const actions = {
-
   async getTodo(context) {
-
     try {
-
-      const result = await axios
-        .get("http://localhost:3000/todo");
+      const result = await instance.get("http://localhost:3000/todo");
 
       console.log("getTodo", result.data);
 
       context.commit("TODO", result.data);
 
       return result;
-
     } catch (error) {
-
       console.error("getTodo", error);
-
-    };
+    }
   },
 
   async createTodo(context, payload) {
-
     try {
-
-      const result = await axios.post("http://localhost:3000/todo", payload);
+      const result = await instance.post("http://localhost:3000/todo", payload);
 
       console.log("createTodo", result.data);
 
       context.dispatch("getTodo");
 
       return result;
-
     } catch (error) {
-
       console.error("createTodo", error);
-
-    };
+    }
   },
 
   async updateTodo(context, payload) {
-
     try {
-
-      const result = await axios.put(`http://localhost:3000/todo/${payload._id}`, payload);
+      const result = await instance.put(
+        `http://localhost:3000/todo/${payload._id}`,
+        payload
+      );
 
       console.log("updateTodo", result.data);
 
       context.dispatch("getTodo");
 
       return result;
-
     } catch (error) {
-
       console.error("updateTodo", error);
-
-    };
+    }
   },
 
   async deleteTodo(context, payload) {
-
     try {
-
-      const result = await axios.delete(`http://localhost:3000/todo/${payload}`);
+      const result = await instance.delete(
+        `http://localhost:3000/todo/${payload}`
+      );
 
       console.log("deleteTodo", result.status);
 
       context.dispatch("getTodo");
 
       return result;
-
     } catch (error) {
-
       console.error("deleteTodo", error);
-
-    };
+    }
   },
 
   async createTodoItem(context, payload) {
-
     try {
-
-      const result = await axios.post(`http://localhost:3000/todo-item/${payload._id}`, payload);
+      const result = await instance.post(
+        `http://localhost:3000/todo-item/${payload._id}`,
+        payload
+      );
 
       console.log("createTodoItem", result.data);
 
       context.dispatch("getTodo");
 
       return result;
-
     } catch (error) {
-
       console.error("createTodoItem", error);
-
-    };
+    }
   },
 
   async updateTodoItem(context, payload) {
-
     try {
-
-      const result = await axios.put(`http://localhost:3000/todo-item/${payload.todoId}/${payload.itemId}`, payload.name);
+      const result = await instance.put(
+        `http://localhost:3000/todo-item/${payload.todoId}/${payload.itemId}`,
+        payload.name
+      );
 
       console.log("updateTodoItem", result.data);
 
       context.dispatch("getTodo");
 
       return result;
-
     } catch (error) {
-
       console.error("updateTodoItem", error);
-
-    };
+    }
   },
 
   async deleteTodoItem(context, payload) {
-
     try {
-
-      const result = await axios.delete(`http://localhost:3000/todo-item/${payload.todoId}/${payload.itemId}`);
+      const result = await instance.delete(
+        `http://localhost:3000/todo-item/${payload.todoId}/${payload.itemId}`
+      );
 
       console.log("deleteTodoItem", result.status);
 
       context.dispatch("getTodo");
 
       return result;
-
     } catch (error) {
-
       console.error("deleteTodoItem", error);
-
-    };
+    }
   },
 
   async signUp(context, payload) {
-
     try {
-
-      return await axios.post("http://localhost:3000/auth/signup", payload);
-
+      return await instance.post("http://localhost:3000/auth/signup", payload);
     } catch (error) {
-
       console.error("signUp", error);
-
-    };
+    }
   },
 
-
   async signIn(context, payload) {
-
     try {
-
-      return await axios.post("http://localhost:3000/auth/signin", payload);
-
+      return await instance.post("http://localhost:3000/auth/signin", payload);
     } catch (error) {
-
       console.error(error);
-
-    };
+    }
   },
 
   async updateUser(context, payload) {
-
     try {
-
-      const result = await axios.put(`http://localhost:3000/user`, payload);
+      const result = await instance.put(`http://localhost:3000/user`, payload);
 
       console.log("updateUser", result.data);
 
       context.dispatch("getUser");
 
       return result;
-
     } catch (error) {
-
       console.error("updateUser", error);
-
-    };
+    }
   },
 
   async getUser(context) {
-
     try {
-
-      const result = await axios
-        .get("http://localhost:3000/user");
+      const result = await instance.get("http://localhost:3000/user");
 
       console.log("getUser", result.data);
 
       context.commit("USER", result.data);
 
       return result;
-
     } catch (error) {
-
       console.error("getUser", error);
-
-    };
+    }
   },
 };
 
